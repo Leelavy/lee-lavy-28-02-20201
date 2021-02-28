5e30200dc5abf558798b002a3f2b5fab55a823ce
leelavy <lilodead@gmail.com>
2021-02-28 16:25:40 +0200

    Implemented searchbar with autocomplete and redux state update with current weather on select

--- src/redux/actions/weatherActions.js
+++ src/redux/actions/weatherActions.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 //should import real api urls after setting up the app
 import { dummyAutocompleteURL, dummyCurrentWeatherURL, dummyFiveDaysWeatherURL } from '../../api/accuWeatherAPI';
 
-export const loadAutoComplete = () => async (dispatch) => {
-  //FETCH AXIOS
-  const autocompleteData = await axios.get(dummyAutocompleteURL());
+export const loadAutoComplete = (q) => async (dispatch) => {
+  //SHOULD BE FETCH AXIOS
+  let autocompleteData = [];
+  if (q === 'te') {
+    autocompleteData = dummyAutocompleteURL();
+  }
+  console.log(autocompleteData);
   dispatch({
     type: "FETCH_AUTOCOMPLETE",
     payload: {
@@ -14,8 +18,8 @@ export const loadAutoComplete = () => async (dispatch) => {
 };
 
 export const loadCurrentWeather = () => async (dispatch) => {
-  //FETCH AXIOS
-  const currentWeatherData = await axios.get(dummyCurrentWeatherURL());
+  //SHOULD BE FETCH AXIOS
+  const currentWeatherData = dummyCurrentWeatherURL();
   dispatch({
     type: "FETCH_CURRENT_WEATHER",
     payload: {
@@ -25,7 +29,8 @@ export const loadCurrentWeather = () => async (dispatch) => {
 };
 
 export const loadFiveDaysWeather = () => async (dispatch) => {
-  const fiveDaysWeatherData = await axios.get(dummyFiveDaysWeatherURL());
+  //SHOULD BE FETCH AXIOS
+  const fiveDaysWeatherData = dummyFiveDaysWeatherURL();
   dispatch({
     type: "FETCH_FIVE_DAYS_WEATHER",
     payload: {
