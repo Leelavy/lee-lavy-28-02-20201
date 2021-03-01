73d3685b7ad0e6448310b733bf22900844e955ec
leelavy <lilodead@gmail.com>
2021-03-02 00:57:53 +0200

    Implemented all current weather, weather by location and days of the week weather fetch logic and favorites functionality

--- src/components/CurrentWeatherSection.js
+++ src/components/CurrentWeatherSection.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToFavorites } from '../redux/actions/favoritesActions';
 import styled from 'styled-components';
 import WeatherCard from './WeatherCard';
 import Sun from '../images/Sun.svg';
+import Cloudy from '../images/Cloudy.svg';
+import Moon from '../images/Moon.svg';
+import Rain from '../images/Rain.svg';
+import Windy from '../images/Windy.svg';
+import Snow from '../images/Snow.svg';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -20,36 +27,85 @@ const useStyles = makeStyles((theme) => ({
 const CurrentWeatherSection = () => {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const currentWeather = useSelector(state => state.weather.currentWeather[0]);
+  const cityDetails = useSelector(state => state.weather.currentCity);
+  const favoriteCities = useSelector(state => state.favorites.favoriteCities);
+
+  const handleFavoriteClick = () => {
+    dispatch(addToFavorites(cityDetails));
+  }
+
+
+  const isFavorite = () => {
+    for (const city of favoriteCities) {
+      if (city.LocalizedName === cityDetails.LocalizedName) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const getWeatherImage = () => {
+    const icon = currentWeather.WeatherIcon;
+    if ((icon >= 1 && icon <= 4) || icon == 32) {
+      return (Sun);
+    }
+    else if (icon >= 5 && icon <= 11) {
+      return (Cloudy);
+    }
+    else if ((icon >= 12 && icon <= 18) || (icon >= 39 && icon <= 40)) {
+      return (Rain);
+    }
+    else if ((icon >= 19 && icon <= 29) || (icon >= 43 && icon <= 44)) {
+      return (Snow);
+    }
+    else if (icon == 31 || icon == 32) {
+      return (Windy);
+    }
+    else if (icon >= 33 || icon <= 38) {
+      return (Moon);
+    }
+    else {
+      return (Cloudy);
+    }
+  }
 
   return (
     <StyledWeatherSection>
-      <StyledDataDiv>
-        <StyledCityName>Tel Aviv</StyledCityName>
-        <StyledCountryDiv>
-          <StyledCountryName>Israel</StyledCountryName>
-          <Line />
-        </StyledCountryDiv>
-        <Degrees>28°</Degrees>
-        <WeatherText>Cloudy</WeatherText>
-        <Precipitation>No Precipitation</Precipitation>
-      </StyledDataDiv>
-      <StyledStatsDiv>
-        <WeatherCard title="Wind" value="25" measureUnit="KM/H" />
-        <WeatherCard title="Humidity" value="75" measureUnit="%" />
-        <WeatherCard title="Realfeel" value="36" measureUnit="°" />
-      </StyledStatsDiv>
-      <StyledWeatherImageDiv>
-        <StyledWeatherImage src={Sun} alt="sun" />
-      </StyledWeatherImageDiv>
-      <Button
-        variant="contained"
-        color="secondary"
-        size="small"
-        className={classes.button}
-        startIcon={<FavoriteIcon />}
-      >
-        Add to favorites
-      </Button>
+      {Object.keys(cityDetails).length !== 0 && (
+        <>
+          <StyledDataDiv>
+            <StyledCityName>{cityDetails.LocalizedName}</StyledCityName>
+            <StyledCountryDiv>
+              <StyledCountryName>{cityDetails.Country.LocalizedName}</StyledCountryName>
+              <Line />
+            </StyledCountryDiv>
+            <Degrees>{currentWeather.Temperature.Metric.Value}°</Degrees>
+            <WeatherText>{currentWeather.WeatherText}</WeatherText>
+            <Precipitation>{currentWeather.HasPrecipitation ? currentWeather.PrecipitationType : "No Precipitations"}</Precipitation>
+          </StyledDataDiv>
+
+          <StyledStatsDiv>
+            <WeatherCard title="Wind" value={currentWeather.Wind.Speed.Metric.Value} measureUnit="km/h" />
+            <WeatherCard title="Humidity" value={currentWeather.RelativeHumidity} measureUnit="%" />
+            <WeatherCard title="Realfeel" value={currentWeather.RealFeelTemperature.Metric.Value} measureUnit="°" />
+          </StyledStatsDiv>
+          <StyledWeatherImageDiv>
+            <StyledWeatherImage src={getWeatherImage()} alt="sun" />
+          </StyledWeatherImageDiv>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="small"
+            className={classes.button}
+            startIcon={<FavoriteIcon />}
+            onClick={handleFavoriteClick}
+          >
+            {isFavorite() ? "Followed" : "Add To Favorites"}
+          </Button>
+        </>
+      )}
     </StyledWeatherSection>
 
   );
